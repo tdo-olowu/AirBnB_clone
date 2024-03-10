15e48b81bb30fc8b74cf27d18b74ffb3ec07d33a
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-10 08:01:15 +0100

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,34 +2,44 @@
 """The base model, from which all other classes will inherit.
 """
 
+import cmd
+from datetime import datetime
+import json
+import uuid
+
 
 class BaseModel:
     """the Base Model class.
         It defines all common attributes/methods for other classes
     """
 
-    def __init__(self, obj_id, created_at, updated_at):
+    def __init__(self, *args, **kwargs):
         """the initialiser
         self.id should assign a uuid4 from uuid module
         """
-        self.id = obj_id
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """print the base in printable format"""
         cls_name = self.__class__.__name__
         my_id = self.id
         my_dct = self.__dict__
-        print("[{}] ({}) {}".format(cls_name, my_id, my_dct))
+        return "[{}] ({}) {}".format(cls_name, my_id, my_dct)
 
     def save(self):
         """updates the public instance attribute updated_at
         with the current datetime"""
-        self.updated_at = 0
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__
         of the instance
         """
-        return {}
+        dct = {}
+        dct = self.__dict__.copy()
+        dct['__class__'] = self.__class__.__name__
+        dct['created_at'] = self.created_at.isoformat()
+        dct['updated_at'] = self.updated_at.isoformat()
+        return dct
