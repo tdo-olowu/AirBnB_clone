76bd3a250780a001e5365f82ed1315d5bdfee0d9
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-10 09:24:15 +0100

    updated

--- .bnb_junkyard/base_model.py
+++ .bnb_junkyard/base_model.py
@@ -17,18 +17,9 @@ class BaseModel:
         """the initialiser
         self.id should assign a uuid4 from uuid module
         """
-        if (kwargs):
-            fmt = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, val in kwargs.items():
-                if key != "__class__":
-                    if key in ("created_at", "updated_at"):
-                        val = datetime.strptime(val, fmt)
-                    setattr(self, key, val)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """print the base in printable format"""
@@ -37,12 +28,10 @@ class BaseModel:
         my_dct = self.__dict__
         return "[{}] ({}) {}".format(cls_name, my_id, my_dct)
 
-
     def save(self):
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
-
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__
